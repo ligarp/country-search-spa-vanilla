521339b880bfc225162c8ce7c871e1e2b4c56d8a
ligarp <om.ragil7@gmail.com>
2020-09-10 21:24:24 +0700

    Done

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import "./styles/style.css";
 import Navigo from "navigo";
 import "./components/country-list";
 import "./components/country-detail";
+import "./components/index-header";
+import "./components/detail-header";
 
 var root = null;
 var useHash = true;
@@ -23,6 +25,7 @@ router
 let data = [];
 
 function renderCountryList() {
+  changeHeader("index-header");
   fetch("https://restcountries.eu/rest/v2/all")
     .then((res) => {
       return res.json();
@@ -42,31 +45,28 @@ function changeList(list) {
   root.appendChild(countryListElement);
 }
 
-const searchInput = document.getElementById("search");
-searchInput.addEventListener("keyup", (event) => {
-  const newCountryList = data.filter(
-    (country) =>
-      country.name.toLowerCase().includes(event.target.value.toLowerCase()) ||
-      country.alpha2Code
-        .toLowerCase()
-        .includes(event.target.value.toLowerCase()) ||
-      country.alpha3Code
-        .toLowerCase()
-        .includes(event.target.value.toLowerCase())
-  );
-  changeList(newCountryList);
-});
+function changeHeader(element, title = "") {
+  const header = document.getElementById("header");
+  const headerElement = document.createElement(`${element}`);
+  if (element == "detail-header") {
+    headerElement.title = title;
+  }
+  header.innerHTML = "";
+  header.appendChild(headerElement);
+}
 
 function showDetails(code) {
+  changeHeader("detail-header");
   const root = document.getElementById("root");
   root.innerHTML = "";
   fetch(`https://restcountries.eu/rest/v2/alpha/${code}`)
     .then((res) => res.json())
     .then((resJson) => {
+      changeHeader("detail-header", resJson.name);
       const root = document.getElementById("root");
       const countryDetailElement = document.createElement("country-detail");
       console.log(countryDetailElement);
-      countryDetailElement.setAttribute("class", "container");
+      countryDetailElement.setAttribute("class", "container detail-container");
       countryDetailElement.country = resJson;
       root.innerHTML = "";
       root.appendChild(countryDetailElement);
--- src/components/country-detail.js
+++ src/components/country-detail.js
@@ -36,23 +36,23 @@ class CountryDetail extends HTMLElement {
             </thead>
             <tbody>
               <tr>
-                <td class="border px-4 py-2">Common</td>
-                <td class="border px-4 py-2">${this._country.name}</td>
+                <td>Common</td>
+                <td>${this._country.name}</td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">Native Name</td>
-                <td class="border px-4 py-2">${this._country.nativeName}</td>
+                <td>Native Name</td>
+                <td>${this._country.nativeName}</td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">Alternative Name</td>
-                <td class="border px-4 py-2">
+                <td>Alternative Name</td>
+                <td>
                 ${this._country.altSpellings.join(", ")}
                 </td>
               </tr>
               <tr></tr>
 
-              <td class="border px-4 py-2">Translations</td>
-              <td class="border px-4 py-2">
+              <td>Translations</td>
+              <td>
               <a href="#!/de"> ${this._country.translations.de} (de)</a> <br>
               <a href="#!/es"> ${this._country.translations.es} (es)</a> <br>
               <a href="#!/fr"> ${this._country.translations.fr} (fr)</a> <br>
@@ -95,28 +95,32 @@ class CountryDetail extends HTMLElement {
             </thead>
             <tbody>
               <tr>
-                <td class="border px-4 py-2">ISO 3166-1 alpha-2</td>
-                <td class="border px-4 py-2">AF</td>
+                <td>ISO 3166-1 alpha-2</td>
+                <td>${this._country.alpha2Code}</td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">ISO 3166-1 alpha-3</td>
-                <td class="border px-4 py-2">AFG</td>
+                <td>ISO 3166-1 alpha-3</td>
+                <td>${this._country.alpha3Code}</td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">ISO 3166-1 numeric</td>
-                <td class="border px-4 py-2">004</td>
+                <td>ISO 3166-1 numeric</td>
+                <td>${this._country.numericCode}</td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">International calling code</td>
-                <td class="border px-4 py-2">93</td>
+                <td>International calling code</td>
+                <td>${this._country.callingCodes.map((num) => {
+                  return num + "<br>";
+                })}</td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">ISO 4217 currency code</td>
-                <td class="border px-4 py-2">AFN</td>
+                <td>Currency</td>
+                <td>${this._country.currencies.map((currency) => {
+                  return currency.code + " (" + currency.symbol + ")";
+                })}</td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">Top level domain</td>
-                <td class="border px-4 py-2">.af</td>
+                <td>Top level domain</td>
+                <td>${this._country.topLevelDomain}</td>
               </tr>
             </tbody>
           </table>
@@ -128,36 +132,39 @@ class CountryDetail extends HTMLElement {
             </thead>
             <tbody>
               <tr>
-                <td class="border px-4 py-2">Region</td>
-                <td class="border px-4 py-2">${
-                  this._country.region ? this._country.region : "-"
-                }</td>
+                <td>Region</td>
+                <td>${this._country.region ? this._country.region : "-"}</td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">Subregion</td>
-                <td class="border px-4 py-2">${
+                <td>Subregion</td>
+                <td>${
                   this._country.subregion ? this._country.subregion : "-"
                 }</td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">Capital Name</td>
-                <td class="border px-4 py-2">${this._country.capital}</td>
+                <td>Capital Name</td>
+                <td>${this._country.capital}</td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">Border</td>
-                <td class="border px-4 py-2">
+                <td>Border</td>
+                <td>
                 ${this._country.borders.map((country) => {
                   return "<a href='#!/" + country + "'>" + country + "</a>";
                 })}
                 </td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">Lat/Long</td>
-                <td class="border px-4 py-2">33, 65</td>
+                <td>Lat/Long</td>
+                <td>
+                <a href="https://www.openstreetmap.org/#map=5/${
+                  this._country.latlng[0]
+                }/${this._country.latlng[1]}" title="Click to view ${
+      this._country.nativeName
+    } on Map">${this._country.latlng.join(", ")}</a></td>
               </tr>
               <tr>
-                <td class="border px-4 py-2">Area</td>
-                <td class="border px-4 py-2">652230km²</td>
+                <td>Area</td>
+                <td>${this._country.area} km²</td>
               </tr>
             </tbody>
           </table>
